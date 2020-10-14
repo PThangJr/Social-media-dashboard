dd2d17339774251f1515dcf87a78d45163261fb3
Phạm Thắng <pthjrr@gmail.com>
2020-10-14 09:13:08 +0700

    Update css: add new animation  and fix button switch in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ const wrapper = document.querySelector("#wrapper");
 const btnSwitch = document.querySelector(".btn--switch");
 
 btnSwitch.addEventListener("change", (e) => {
-    wrapper.classList.toggle("dark-mode");
+    if (e.target.checked) {
+        wrapper.classList.add("dark-mode");
+    }
+    else {
+        wrapper.classList.remove("dark-mode");        
+    }
     
 })
